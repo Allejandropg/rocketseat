35c448479f651d4a0d4aee28ee3ccee94de30c38
Allejandropg <alle401@gmail.com>
2019-10-15 07:41:51 -0300

    inicio desafio01

--- bootcamp/desafio01/index.js
+++ bootcamp/desafio01/index.js
@@ -1,9 +1,28 @@
+// https://github.com/Rocketseat/bootcamp-gostack-desafio-01/blob/master/README.md#desafio-01-conceitos-do-nodejs
 const express = require('express');
 
 
 const server = express();
-server.use(express.json())
+server.use(express.json());
+const projetos = [];
 
+//listar todos os projetos
+server.get('/projects',(req,res) => {
+    return res.json(projetos);
+});
 
+//pega um projeto pelo id
+server.get('/projects/:id',(req,res) => {
+    const { id } = req.params;
+    return res.json(projetos[id]);
+});
+
+//cadastra um projeto
+server.post('/projects',(req, res) => {
+    const { id, projeto } = req.body;
+
+    projetos.push(req.body);
+    console.log(projetos)
+});
 
 server.listen(3001);//localhost:3001
