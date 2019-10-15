038b22b2679bbbca7afe3b9f396229e1129980f3
Allejandropg <49108239+Allejandropg@users.noreply.github.com>
2019-10-15 13:52:21 -0300

    Update index.js
    alteração de rotas e implementação do delete

--- bootcamp/desafio01/index.js
+++ bootcamp/desafio01/index.js
@@ -11,18 +11,50 @@ server.get('/projects',(req,res) => {
     return res.json(projetos);
 });
 
+//cadastra um projeto
+server.post('/projects',(req, res) => {
+    const aux = req.body;
+    aux.tasks = [];
+    projetos.push(aux);
+    return res.send();
+});
+
+//altera um projeto com base no id
+server.put('/projects/:id',(req, res) =>{
+    const id = req.params.id;
+    const title = req.body.title;
+    const projeto = getByIndex(id);
+    projeto.title = title;
+    return res.send();
+});
+//altera um projeto com base no id
+server.delete('/projects/:id',(req, res) =>{
+    const id = req.params.id;
+    const index = getIndex(id);
+    projetos.splice(index,1);
+    return res.send();
+});   
+//pega um projeto pelo id
+server.post('/projects/:id/tasks',(req,res) => {
+    const id = req.params.id;
+    const { task } = req.body;
+    const projeto = getByIndex(id);
+    projeto.task.push(task)
+    return res.send();
+});
+
+           
 //pega um projeto pelo id
 server.get('/projects/:id',(req,res) => {
     const { id } = req.params;
-    return res.json(projetos[id]);
+    return res.json(getByIndex(id));
 });
 
-//cadastra um projeto
-server.post('/projects',(req, res) => {
-    const { id, projeto } = req.body;
-
-    projetos.push(req.body);
-    console.log(projetos)
-});
-
+const getByIndex = (id) => {
+    const index = projetos.findIndex(projeto => projeto.id==id);
+    return projetos[index];
+}
+const getIndex = (id) => {
+    return projetos.findIndex(projeto => projeto.id==id)
+}
 server.listen(3001);//localhost:3001
