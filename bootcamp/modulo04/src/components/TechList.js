2f078fc06fdc628e7e7328e47d1917977d8412a1
Allejandropg <alle401@gmail.com>
2019-11-07 07:34:51 -0300

    init module04

---
+++ bootcamp/modulo04/src/components/TechList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+class TechList extends Component {
+  state = {
+    techs:[
+      'Node.Js',
+      'ReactJS',
+      'REact Native'
+    ]
+  }
+  render(){
+    return (
+      <ul>
+        <li>Node.js</li>
+        <li>ReactJS</li>
+        <li>React Native</li>
+      </ul>
+    );
+  }
+}
+
+export default TechList;
