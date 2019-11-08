a628b31d37bc82ab85094f493f007dca2e7a3465
Allejandropg <alle401@gmail.com>
2019-11-08 09:26:09 -0300

    Finishing the module04

--- bootcamp/modulo04/src/components/TechList.js
+++ bootcamp/modulo04/src/components/TechList.js
@@ -1,19 +1,60 @@
 import React, { Component } from 'react'
+import TechItem from './TechItem';
+
 class TechList extends Component {
   state = {
-    techs:[
-      'Node.Js',
-      'ReactJS',
-      'REact Native'
-    ]
+    newTech:'',
+    techs:[]
   }
+
+  // Executa assim que o componente aparece em tela
+  componentDidMount(){
+    const techs =  localStorage.getItem('techs');
+
+    if(techs){
+      this.setState({techs: JSON.parse(techs)});
+    }
+  }
+  
+  // Executa sempre que houver alteração nas props ou estado
+  componentDidUpdate(_, prevState){
+    // para saber se mudou basta comparar com this.props(prevProps) ou this.state(prevState)
+    if(this.state.techs !== prevState.techs){
+      localStorage.setItem('techs', JSON.stringify(this.state.techs))
+    }
+  }
+
+  // Executa quando o componente deixa de existir
+  componentWillUnmount(){
+
+  }
+
+  handleInputChange = e => {
+    this.setState({newTech:e.target.value})
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({techs: [...this.state.techs,this.state.newTech], newTech:''})
+  }
+
+  handleDelete = tech => {
+    this.setState({techs: this.state.techs.filter( t => t !== tech )})
+  }
+
   render(){
     return (
-      <ul>
-        <li>Node.js</li>
-        <li>ReactJS</li>
-        <li>React Native</li>
-      </ul>
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map(tech => <TechItem key={tech} tech={tech} onDelete={() => this.handleDelete(tech)} />)}
+        </ul>
+        <input 
+          type="text" 
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        />
+        <button type="submit">Enviar</button>
+      </form>
     );
   }
 }
