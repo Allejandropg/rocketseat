0d9c51868d3c29f54b6bf50d2726b6674e768cbe
Allejandropg <alle401@gmail.com>
2019-11-10 22:12:07 -0300

    small changes

--- bootcamp/modulo05/src/App.js
+++ bootcamp/modulo05/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 import Routes from './routes';
+import GlobalStyled from './styles/global';
 
 function App() {
-  return <Routes />;
+  return (
+    <>
+      <Routes />
+      <GlobalStyled />
+    </>
+  );
 }
 
 export default App;
