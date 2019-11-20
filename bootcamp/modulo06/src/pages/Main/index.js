f24800d1394e61b7b1b50dcbaff15a28725172dc
Allejandropg <alle401@gmail.com>
2019-11-20 15:00:17 -0300

    try make loadmore

--- bootcamp/modulo06/src/pages/Main/index.js
+++ bootcamp/modulo06/src/pages/Main/index.js
@@ -1,37 +1,95 @@
 import React, { Component } from 'react';
-import { Keyboard } from 'react-native';
+import PropTypes from 'prop-types';
+import { Keyboard, ActivityIndicator } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '../../services/api';
 
-import { Container, Form, Input, SubmitButton, List } from './styles';
+import {
+  Container,
+  Form,
+  Input,
+  SubmitButton,
+  ClearButton,
+  ClearButtonText,
+  List,
+  User,
+  Avatar,
+  Name,
+  Bio,
+  ProfileButton,
+  ProfileButtonText,
+} from './styles';
 
 export default class Main extends Component {
+  static propTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func,
+    }).isRequired,
+  };
+
   state = {
     newUser: '',
     users: [],
+    loading: false,
+    loadTrash: false,
   };
+
+  async componentDidMount() {
+    const users = await AsyncStorage.getItem('users');
+
+    if (users) {
+      this.setState({ users: JSON.parse(users) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { users } = this.state;
+    if (prevState.users !== users) {
+      AsyncStorage.setItem('users', JSON.stringify(users));
+    }
+  }
 
   handleAddUser = async () => {
     const { users, newUser } = this.state;
-    const response = await api.get(`/user/${newUser}`);
+    this.setState({ loading: true });
+
+    const response = await api.get(`/users/${newUser}`);
 
     const data = {
       name: response.data.name,
       login: response.data.login,
-      bioi: response.data.bio,
+      bio: response.data.bio,
       avatar: response.data.avatar_url,
     };
 
     this.setState({
       users: [...users, data],
       newUser: '',
+      loading: false,
     });
 
     Keyboard.dismiss();
   };
 
+  handleClearAll = async () => {
+    this.setState({ loadTrash: true });
+    await AsyncStorage.clear();
+    this.setState({ loadTrash: false, users: [] });
+  };
+
+  handleNavigate = user => {
+    const { navigation } = this.props;
+
+    navigation.navigate('User', { user });
+  };
+
+  static navigationOptions = {
+    title: 'Usuários',
+  };
+
   render() {
-    const { users, newUser } = this.state;
+    const { users, newUser, loading, loadTrash } = this.state;
 
     return (
       <Container>
@@ -42,14 +100,39 @@ export default class Main extends Component {
             placeholder="Adicionar Usuário"
             value={newUser}
             onChangeText={text => this.setState({ newUser: text })}
-            returnKeyType="Send"
             onSubmitEditing={this.handleAddUser}
           />
-          <SubmitButton onPress={this.handleAddUser}>
-            <Icon name="add" size={20} />
+          <SubmitButton loading={loading} onPress={this.handleAddUser}>
+            {loading ? (
+              <ActivityIndicator color="#FFF" />
+            ) : (
+              <Icon name="add" size={20} color="#FFF" />
+            )}
           </SubmitButton>
         </Form>
-        <List />
+        <Form>
+          <ClearButton loading={loadTrash} onPress={this.handleClearAll}>
+            {loadTrash ? (
+              <ActivityIndicator color="#FFF" />
+            ) : (
+              <ClearButtonText>Remover Usários</ClearButtonText>
+            )}
+          </ClearButton>
+        </Form>
+        <List
+          data={users}
+          keyExtractor={user => user.login}
+          renderItem={({ item }) => (
+            <User>
+              <Avatar source={{ uri: item.avatar }} />
+              <Name>{item.name}</Name>
+              <Bio>{item.bio}</Bio>
+              <ProfileButton onPress={() => this.handleNavigate(item)}>
+                <ProfileButtonText> Ver Perfil</ProfileButtonText>
+              </ProfileButton>
+            </User>
+          )}
+        />
       </Container>
     );
   }
--- bootcamp/modulo06/src/pages/User/index.js
+++ bootcamp/modulo06/src/pages/User/index.js
@@ -1,8 +1,110 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { ActivityIndicator } from 'react-native';
+import api from '../../services/api';
+
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Loading,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
 
 // import { Container } from './styles';
 
-export default function User() {
-  return <View />;
+export default class User extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('user').name,
+  });
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
+    }).isRequired,
+  };
+
+  state = {
+    stars: [],
+    loading: true,
+    page: 1,
+  };
+
+  async componentDidMount() {
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+    const perPage = 8;
+    const { page } = this.state;
+
+    const response = await api.get(`/users/${user.login}/starred`, {
+      params: {
+        per_page: perPage,
+        page,
+      },
+    });
+    this.setState({ stars: response.data, loading: false, page: page + 1 });
+  }
+
+  async loadMore() {
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+    const perPage = 8;
+
+    const { page } = this.state;
+    const response = await api.get(`/users/${user.login}/starred`, {
+      params: {
+        per_page: perPage,
+        page,
+      },
+    });
+    console.tron.log(response.data);
+    this.setState({
+      stars: [response.data],
+      loading: false,
+      page: page + 1,
+    });
+  }
+
+  render() {
+    const { navigation } = this.props;
+    const { stars, loading } = this.state;
+    const user = navigation.getParam('user');
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+        {loading ? (
+          <Loading>
+            <ActivityIndicator color="#7159C1" size={100} />
+          </Loading>
+        ) : (
+          <Stars
+            onEndReachedThreshold={0.2} // Carrega mais itens quando chegar em 20% do fim
+            onEndReached={() => this.loadMore()} // Função que carrega mais itens
+            data={stars}
+            keyExtrator={star => String(star.id)}
+            renderItem={({ item }) => (
+              <Starred>
+                <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+                <Info>
+                  <Title>{item.name}</Title>
+                  <Author>{item.owner.login}</Author>
+                </Info>
+              </Starred>
+            )}
+          />
+        )}
+      </Container>
+    );
+  }
 }
